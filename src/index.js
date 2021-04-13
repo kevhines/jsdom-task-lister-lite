968267e84ebb891e5bf6281b02afaf58efd74c66
Kevin <kevhines@gmail.com>
2021-04-13 17:26:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
+    newTask = document.getElementById("new-task-description").value
+    console.log(newTask);
+    const node = document.createElement("LI");
+    const textnode = document.createTextNode(newTask);         // Create a text node
+    node.appendChild(textnode);   
+    document.getElementById("tasks").appendChild(node);
+    //tasks.appendChild.innerHTML = "hello";
+    event.preventDefault();
+}, false);
 });
